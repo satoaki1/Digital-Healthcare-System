bb59300d36afd6e63eccee28aee81d7d02502f54
ihsa332019 <satoakiishihara@sd.taylors.edu.my>
2023-06-25 11:32:49 +0800

    Updated Dashboard.fxml and DashboardController

--- src/main/java/com/example/digitalhealthcaresystem/DashboardController.java
+++ src/main/java/com/example/digitalhealthcaresystem/DashboardController.java
@@ -1,18 +1,23 @@
 package com.example.digitalhealthcaresystem;
 
+import com.example.digitalhealthcaresystem.PatientForms.ViewPatientInformationFormController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class DashboardController {
+    private static final String FILE_PATH = "patients.txt";
+
     @FXML
     private Hyperlink medicalHistoryLink;
 
@@ -29,9 +34,6 @@ public class DashboardController {
     private Hyperlink dataStorageSystemLink;
 
     @FXML
-    private Button goToHomeButton;
-
-    @FXML
     private Button goToCalenderButton;
 
     @FXML
@@ -41,7 +43,13 @@ public class DashboardController {
     private Button exitButton;
 
     @FXML
-    public void handleMedicalHistoryLink(ActionEvent event) {
+    private TextField patientField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    public void handlePatientHistoryLink(ActionEvent event) {
         PatientHistoryFormController patientHistoryFormController = new PatientHistoryFormController();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         patientHistoryFormController.showPatientHistoryForm(stage);
@@ -76,23 +84,82 @@ public class DashboardController {
     }
 
     @FXML
-    public void handleGoToHomeButton() {
-        goToHomeButton.setOnAction(event -> System.out.println("goToHomeButton"));
+    public void handleGoToDashboardButton(ActionEvent event) {
+        DashboardController dashboardController = new DashboardController();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        dashboardController.loadDashboardView(stage);
     }
 
     @FXML
-    public void handleGoToDashboardButton() {
-        goToDashboardButton.setOnAction(event -> System.out.println("goToDashboardButton"));
+    public void handleGoToCalenderButton(ActionEvent event) {
+        CalenderFormController calenderFormController = new CalenderFormController();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        calenderFormController.showCalenderForm(stage);
     }
 
     @FXML
-    public void handleGoToCalenderButton() {
-        goToCalenderButton.setOnAction(event -> System.out.println("goToCalenderButton"));
+    public void handleExitButton(ActionEvent event) {
+        showExitConfirmation();
     }
 
     @FXML
-    public void handleExitButton() {
-        exitButton.setOnAction(event1 -> System.exit(1));
+    public void handleSearchButton(ActionEvent event) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String patientName = patientField.getText();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                if (!data[1].equals(patientName)) {
+                    continue;
+                } else {
+                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                    try {
+                        // Correctly point to the FXML file
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/digitalhealthcaresystem/PatientFormDocuments/ViewPatientInformationForm.fxml"));
+                        Parent root = loader.load();
+
+                        // Create a new scene with the root parent
+                        Scene scene = new Scene(root);
+
+                        // Set the scene on the primary stage
+                        stage.setScene(scene);
+                        stage.setTitle("Digital Healthcare System");
+
+                        // Show the primary stage
+                        stage.show();
+
+                        // Get the controller
+                        ViewPatientInformationFormController viewPatientInformationFormController = loader.getController();
+
+                        viewPatientInformationFormController.searchPatient(patientName);
+                        break;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showExitConfirmation() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Exit Confirmation");
+        alert.setHeaderText("Unsaved Changes");
+        alert.setContentText("Are you sure you want to exit without saving?");
+
+        ButtonType exitButton = new ButtonType("Exit");
+        ButtonType cancelButton = new ButtonType("Cancel");
+
+        alert.getButtonTypes().setAll(exitButton, cancelButton);
+
+        alert.showAndWait().ifPresent(buttonType -> {
+            if (buttonType == exitButton) {
+                System.exit(0); // Replace this with your application's exit logic
+            }
+        });
     }
 
     @FXML
@@ -101,6 +168,7 @@ public class DashboardController {
             // Load the FXML file and create a root parent
             FXMLLoader loader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
             Parent root = loader.load();
+            DashboardController dashboardController = loader.getController();
 
             // Create a new scene with the root parent
             Scene scene = new Scene(root);
