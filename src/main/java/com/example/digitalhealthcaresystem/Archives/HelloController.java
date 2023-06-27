981f3dec582f08c5283718d20824a4a71c633fa4
ihsa332019 <satoakiishihara@sd.taylors.edu.my>
2023-06-27 23:00:35 +0800

    Updated Project.

--- src/main/java/com/example/digitalhealthcaresystem/Archives/HelloController.java
+++ src/main/java/com/example/digitalhealthcaresystem/Archives/HelloController.java
@@ -2,4 +2,5 @@ package com.example.digitalhealthcaresystem.Archives;
 
 public class HelloController {
     // This is the Controller to execute the program without issues.
+    // Since this controller is set in default, it could not be deleted.
 }
