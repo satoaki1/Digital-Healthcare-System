95ad8f9291a959bb2eedd2cd11e4ae67a1660ce6
ihsa332019 <satoakiishihara@sd.taylors.edu.my>
2023-06-28 21:10:14 +0800

    Updated Project.

--- src/main/java/com/example/digitalhealthcaresystem/Archives/HelloController.java
+++ src/main/java/com/example/digitalhealthcaresystem/Archives/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.digitalhealthcaresystem.Archives;
 
 public class HelloController {
-    // This is the Controller to execute the program without issues.
-    // Since this controller is set in default, it could not be deleted.
+    // The HelloController within the Archives package is intentionally kept in order to ensure the successful startup
+    // of the application. Since this controller is set in default, it could not be deleted. Despite multiple attempts
+    // to remove this class, modifying the module-info.java file did not eliminate the errors, and warnings were
+    // consistently received from the console, urging for its presence. Therefore, it has been retained, although this
+    // class does not possess any special functionality or significance.
 }
